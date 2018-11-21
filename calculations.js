79b97197c4283e2053b8263766307512c894c11f
edwincarbajal <exaviercarbajal@gmail.com>
2018-11-21 13:42:53 -0500

    Create function to calculate expressions and update the display

--- calculations.js
+++ calculations.js
@@ -1,18 +1,28 @@
 var calculation_span = document.getElementById("calculation-span")
-//This is the grouping for the numbers
+var product_display = document.getElementById("product-display")
+
 document.querySelectorAll(".number-button").forEach(function(button) {
   button.addEventListener("click", function() {
-    if(parseInt(calculation_span.textContent) !==0){
-      calculation_span.innerHTML += button.textContent
-    } else{
-        calculation_span.innerHTML = button.textContent
+    if (parseInt(calculation_span.textContent) !== 0) {
+      calculation_span.textContent += button.textContent
+    } else {
+      calculation_span.textContent = button.textContent
     }
-    
- })
-
+  })
 })
+
 document.querySelectorAll(".operation-button").forEach(function(button) {
   button.addEventListener("click", function() {
-    calculation_span.innerHTML += button.textContent
+    calculation_span.textContent += ' ' + button.textContent + ' '
   })
+})
+
+calculation_span.addEventListener('DOMSubtreeModified', function() {
+  // Find the last character in the calculation_span string // e.x ('1 + z') --> the last character is 'z'
+  // Check if the last character is equal to a number && return a boolean (true or false) // e.x ('z' === Number) --> false
+  // if true, then evaluate the expression
+    // and update the product_display element
+  if(!!parseInt(calculation_span.textContent[calculation_span.textContent.length - 1])) {
+    product_display.textContent = eval(calculation_span.textContent)
+  }
 })
