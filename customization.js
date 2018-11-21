803f94a7c33583e0cea2a4c3485cd11a18805fdb
chuantian Lin <llcttt@gmail.com>
2018-11-20 21:45:27 -0500

    I wrote the color changing functionality for the canvas

---
+++ customization.js
@@ -0,0 +1,16 @@
+var button_elements = document.querySelectorAll(".canvas-background")
+var center_column = document.getElementById("center_column");
+
+function addOrRemoveClassName(element, button_id) {
+  if(element.classList.contains(element.dataset.bgColor)) {
+    center_column.classList.remove(element.dataset.bgColor);
+    center_column.classList.add(button_id)
+    center_column.dataset.bgColor = button_id;
+  }
+}
+
+button_elements.forEach(function(button) {
+  button.addEventListener("click", function(){
+    addOrRemoveClassName(center_column, button.id)
+  })
+})
