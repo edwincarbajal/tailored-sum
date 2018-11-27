1f0dfc660ecbb61e9fb4b7f500ffef2df1fe1237
chuantian Lin <llcttt@gmail.com>
2018-11-27 13:02:29 -0500

    I did the theme and the display background also I changed the text color to grey

--- customization.js
+++ customization.js
@@ -3,12 +3,17 @@ var all_clear_buttons = document.querySelectorAll(".ac-background");
 var clear_entry_buttons = document.querySelectorAll('.ce-background');
 var operator_button_options = document.querySelectorAll('.operator-background');
 var equals_button_options = document.querySelectorAll('.equals-background');
+var theme_options = document.querySelectorAll('.calculator-background');
+var display_options = document.querySelectorAll('.dispay-background');
+
 
 var center_column = document.getElementById("center_column");
 var all_clear_button = document.getElementById("all-clear");
 var clear_entry_button = document.getElementById("clear-entry");
 var equals_button = document.getElementById("equals-button");
 var operator_buttons = document.querySelectorAll(".operation-button");
+var calculator_theme = document.getElementById("calcualtor-theme");
+var display_screen = document.getElementById("display-screen");
 
 function addOrRemoveClassName(element, button_id) {
   if (element.classList.contains(element.dataset.bgColor)) {
@@ -42,10 +47,23 @@ operator_button_options.forEach(function(button) {
       addOrRemoveClassName(operator, button.id)
     })
   })
+
+  equals_button_options.forEach(function(button) {
+    button.addEventListener("click", function() {
+      addOrRemoveClassName(equals_button, button.id)
+    })
+  })
+
+
+theme_options.forEach(function(button) {
+  button.addEventListener("click", function() {
+    addOrRemoveClassName(calculator_theme, button.id)
+  })
 })
 
-equals_button_options.forEach(function(button) {
+display_options.forEach(function(button) {
   button.addEventListener("click", function() {
-    addOrRemoveClassName(equals_button, button.id)
+    addOrRemoveClassName(display_screen, button.id)
+    })
   })
 })
