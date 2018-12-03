610abd10006588ebe680848fecb726fed648568a
edwincarbajal <exaviercarbajal@gmail.com>
2018-12-03 18:07:17 -0500

    Add operations to local storage and display to the user

--- history.js
+++ history.js
@@ -15,32 +15,37 @@ equals_operator.addEventListener('click', () => {
   } else {
     // store math object in a variable
     const storage_object = JSON.parse(localStorage.getItem('math'));
+
+    // store the new entry at the end of previous entries
     localStorage.setItem('math', JSON.stringify({
-      // store the new entry,
-      [calculation_display.textContent]: products_display.textContent,
-      // add the previous entries
-      ...storage_object
-    }));
+      [calculation_display.textContent]: products_display.textContent, ...storage_object  }));
   }
-  calculation_display.textContent = products_display.textContent
+  // Change the calculations span content with the total, commas removed
+  calculation_display.textContent = products_display.textContent.replace(/\,/g,'')
+  // Call function to populate the history column
   populateHistoryCol()
 })
 
 const populateHistoryCol = () => {
+  // Remove the 'no history message' from html
   if(document.getElementById('no-history')) {
     document.getElementById('no-history').remove()
   }
+  // Get stored math object from local storage and parse it
   const parsedObject = JSON.parse(localStorage.getItem('math'));
-  history_container.innerHTML += `
-    <li class="list-group-item bg-light text-right">
-      <div class="w-100">
-        <small>${ parsedObject[Object.keys(parsedObject)[0]] }</small>
-        <div class="row pt-2">
-          <div class="col">
-            ${ Object.keys(parsedObject)[0] }
+  // Add new html with the math operations to the top of the history container
+  history_container.insertAdjacentHTML('afterbegin',
+    `
+      <li class="list-group-item bg-light text-right">
+        <div class="w-100">
+          <small>${ parsedObject[Object.keys(parsedObject)[0]] }</small>
+          <div class="row pt-2">
+            <div class="col">
+              ${ Object.keys(parsedObject)[0] }
+            </div>
           </div>
         </div>
-      </div>
-    </li>
-  `
+      </li>
+    `
+  )
 }
